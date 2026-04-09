d88e3eaabea0d76190af4084f85443854a5f96c3
faisaldp123 <faisal.dpathshala@gmail.com>
2026-04-09 16:12:28 +0530

    made new changes in category and product page in modals folder

--- models/Category.js
+++ models/Category.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
-const categorySchema = new mongoose.Schema({
-  name: String,
-  slug: String
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// auto slug
+categorySchema.pre("save", function (next) {
+  if (this.name) {
+    this.slug = slugify(this.name, { lower: true });
+  }
+  next();
 });
 
 module.exports = mongoose.model("Category", categorySchema);
